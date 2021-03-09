6101d6fd18679307602c8f7912d1b3144dd3f555
Vfeuer <xinhuang.gong@fau.de>
2021-03-09 20:27:36 +0100

    Update loadstation.sql

--- conf/loadstation.sql
+++ conf/loadstation.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: loadstation
 -- ------------------------------------------------------
--- Server version	5.5.53
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,18 +23,28 @@ DROP TABLE IF EXISTS `meshsetting`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `meshsetting` (
-  `id` int(1) NOT NULL DEFAULT '1',
-  `wholeMax` int(5) DEFAULT '0',
-  `usedCur1` int(5) DEFAULT NULL,
-  `usedCur2` int(5) DEFAULT NULL,
-  `usedCur3` int(5) DEFAULT NULL,
-  `totalCur1` int(5) DEFAULT NULL,
-  `totalCur2` int(5) DEFAULT NULL,
-  `totalCur3` int(5) DEFAULT NULL,
+  `id` int NOT NULL DEFAULT '1',
+  `wholeMax` int DEFAULT '0',
+  `usedCur1` int DEFAULT NULL,
+  `usedCur2` int DEFAULT NULL,
+  `usedCur3` int DEFAULT NULL,
+  `totalCur1` int DEFAULT NULL,
+  `totalCur2` int DEFAULT NULL,
+  `totalCur3` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `meshsetting`
+--
+
+LOCK TABLES `meshsetting` WRITE;
+/*!40000 ALTER TABLE `meshsetting` DISABLE KEYS */;
+INSERT INTO `meshsetting` VALUES (1,100,0,0,0,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `meshsetting` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `nodestatus`
@@ -44,21 +54,31 @@ DROP TABLE IF EXISTS `nodestatus`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `nodestatus` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `macADR` varchar(18) DEFAULT NULL,
-  `workStatus` int(5) DEFAULT NULL,
-  `maxCur` int(5) DEFAULT NULL,
-  `cmaxCur` int(5) DEFAULT NULL,
+  `workStatus` int DEFAULT NULL,
+  `maxCur` int DEFAULT NULL,
+  `cmaxCur` int DEFAULT NULL,
   `workmode` varchar(10) DEFAULT 'normal',
-  `connect` tinyint(4) DEFAULT '0',
+  `connect` tinyint DEFAULT '0',
   `nodeName` varchar(25) DEFAULT NULL,
-  `Cur1` int(5) DEFAULT NULL,
-  `Cur2` int(5) DEFAULT NULL,
-  `Cur3` int(5) DEFAULT NULL,
-  `Phases` int(5) DEFAULT NULL,
+  `Cur1` int DEFAULT NULL,
+  `Cur2` int DEFAULT NULL,
+  `Cur3` int DEFAULT NULL,
+  `Phases` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nodestatus`
+--
+
+LOCK TABLES `nodestatus` WRITE;
+/*!40000 ALTER TABLE `nodestatus` DISABLE KEYS */;
+INSERT INTO `nodestatus` VALUES (6,'d8a01d56b864',0,NULL,10,'auto',1,NULL,0,0,0,12);
+/*!40000 ALTER TABLE `nodestatus` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -68,11 +88,21 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `username` varchar(25) CHARACTER SET utf8mb4 NOT NULL,
-  `password` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
+  `username` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`username`,`password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('admin','79625b4c690b70f10c7803bd8ae99d22');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -83,4 +113,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-09  0:43:16
+-- Dump completed on 2021-03-09 20:25:44

38e78c02f202706f36c794fd901580abcad6af14
Vfeuer <xinhuang.gong@fau.de>
2021-03-09 02:34:06 +0100

    Update loadstation.sql

--- conf/loadstation.sql
+++ conf/loadstation.sql
@@ -25,22 +25,16 @@ DROP TABLE IF EXISTS `meshsetting`;
 CREATE TABLE `meshsetting` (
   `id` int(1) NOT NULL DEFAULT '1',
   `wholeMax` int(5) DEFAULT '0',
-  `safeMax` int(5) DEFAULT '0',
-  `averageMax` int(5) DEFAULT '0',
+  `usedCur1` int(5) DEFAULT NULL,
+  `usedCur2` int(5) DEFAULT NULL,
+  `usedCur3` int(5) DEFAULT NULL,
+  `totalCur1` int(5) DEFAULT NULL,
+  `totalCur2` int(5) DEFAULT NULL,
+  `totalCur3` int(5) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `meshsetting`
---
-
-LOCK TABLES `meshsetting` WRITE;
-/*!40000 ALTER TABLE `meshsetting` DISABLE KEYS */;
-INSERT INTO `meshsetting` VALUES (1,100,32,0);
-/*!40000 ALTER TABLE `meshsetting` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `nodestatus`
@@ -50,28 +44,21 @@ DROP TABLE IF EXISTS `nodestatus`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `nodestatus` (
-  `id` int(3) NOT NULL,
-  `maxCurrent` int(3) DEFAULT NULL,
-  `CurrentValue` int(3) DEFAULT NULL,
-  `workStatus` tinyint(2) unsigned DEFAULT '1',
-  `workmode` varchar(10) DEFAULT 'normal',
-  `connect` tinyint(4) DEFAULT '1',
-  `nodeName` varchar(25) DEFAULT NULL,
-  `ipADR` varchar(15) DEFAULT NULL,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `macADR` varchar(18) DEFAULT NULL,
+  `workStatus` int(5) DEFAULT NULL,
+  `maxCur` int(5) DEFAULT NULL,
+  `cmaxCur` int(5) DEFAULT NULL,
+  `workmode` varchar(10) DEFAULT 'normal',
+  `connect` tinyint(4) DEFAULT '0',
+  `nodeName` varchar(25) DEFAULT NULL,
+  `Cur1` int(5) DEFAULT NULL,
+  `Cur2` int(5) DEFAULT NULL,
+  `Cur3` int(5) DEFAULT NULL,
+  `Phases` int(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `nodestatus`
---
-
-LOCK TABLES `nodestatus` WRITE;
-/*!40000 ALTER TABLE `nodestatus` DISABLE KEYS */;
-INSERT INTO `nodestatus` VALUES (1,32,0,1,'fast',1,'test','192.168.172.44','00:01:6C:06:A6:29'),(2,32,20,1,'normal',1,NULL,'192.168.172.45','FF:FF:FF:FF:FF:FF'),(3,16,20,1,'normal',1,'test2','192.168.172.46','DD:DD:DD:DD:DD:DD'),(12,32,0,0,'normal',1,'test3','255.255.255.255','66:DD:66:DD:66:DD');
-/*!40000 ALTER TABLE `nodestatus` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -86,16 +73,6 @@ CREATE TABLE `user` (
   PRIMARY KEY (`username`,`password`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('admin','79625b4c690b70f10c7803bd8ae99d22');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -106,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-13  0:19:20
+-- Dump completed on 2021-03-09  0:43:16
